9189a6b7a322e32b8ed9e0a6f95e8abd0853527e
Fran <francesc.parra@gmail.com>
2021-05-14 22:20:47 +0200

    no voy a terminar en mi puta vida :(

---
+++ style.js
@@ -0,0 +1,9 @@
+function collapseExpanse(){         
+    document.activeElement.querySelector('i').classList.toggle("collapse");
+    document.activeElement.parentNode.parentNode.querySelector('ul').classList.toggle("visible")
+}
+
+
+
+
+// dropdownChildCollapse
