02b73af3987ba93b2def158301384f0e4e8165c5
Fran <francesc.parra@gmail.com>
2021-05-15 20:35:57 +0200

    cambio position article

--- style.js
+++ style.js
@@ -1,9 +1,14 @@
-function collapseExpanse(){         
+let collapseExpanse= ()=>{         
     document.activeElement.querySelector('i').classList.toggle("collapse");
     document.activeElement.parentNode.parentNode.querySelector('ul').classList.toggle("visible")
 }
 
 
+let inputNumber = (number)=>{
+    document.activeElement.parentNode.querySelector("#input-number").stepUp(number);
+    console.log(`hola que ase me pone el numero ${number} o k ase`)
+    // console.log(document.activeElement.parentNode.getElementById("input-number"))
+}
 
 
 // dropdownChildCollapse
